0aefc7cd0e0eb0ed8451a7add18957f1da94ee73
chiefton117 <timjharrold@gmail.com>
2020-10-06 19:35:52 -0400

    Toggles Fixed
    -Fixed artist toggling
    -Added tabs for genre toggling
    -Working on revised artist selection

--- stats.js
+++ stats.js
@@ -1,69 +1,69 @@
+var hidden = [];
 function genStats() {
-
-	var labels = d3.select("#statlist").selectAll("input")
+	
+	
+	var labels = d3.select("#atoggle").selectAll("input")
 		.data(window.artistData.nodes)
 		.enter().append("label")
 		.attr("class", "col-sm-4 col-md-4 col-lg-4")
-		.text(function(d) { return d.id; });
+		.text(function(d) { return d.id + " "; });
 
 	var	boxes = labels.append("input")
 		.attr("type", "checkbox")
-		.attr("checked", "true")
+		.property("checked", true)
 		.attr("id", function(d) { return d.id; });
-	/*
+	
+	d3.select("#atoggle").append("label") // Select All Button
+		.attr("class", "col-sm-4 col-md-4 col-lg-4")
+		.text("Select All ")
+		.append("input")
+		.attr("type", "checkbox")
+		.property("checked", true)
+		.attr("id", "all")
+		.on("click", function(d) { // Logic for selecting and de-selecting all
+			hidden = [];
+			if(this.hasAttribute("checked")) {
+				this.removeAttribute('checked');
+				$('label>input').prop("checked", false);
+				d3.selectAll(".nodes>g").style("visibility", "hidden");
+				d3.selectAll(".links>line").style("visibility", "hidden");
+				window.artistData.nodes.forEach(e => hidden.push(e.id));
+			} else {
+				this.setAttribute("checked", "true");
+				$('label>input').prop("checked", true);
+				d3.selectAll(".nodes>g").style("visibility", "visible");
+				d3.selectAll(".links>line").style("visibility", "visible");
+			}
+		});
+
+
 	boxes.on("click", function(d) {
 
-		d3.select(this).attr("checked", function(d) {
-
-		});
+		var ch = $(this).prop('checked');
+		var circles = d3.selectAll(".nodes>g");
+	
+		circles.select(function(i) {
+			if(i.id == d.id) return this;
+		}).style("visibility", function(d_sel) {
+				if(ch) {
+					hidden.splice(hidden.indexOf(d_sel.id),1);
+					return 'visible';	
+				}
+				else {
+					hidden.push(d_sel.id);
+					return 'hidden';
+				}
+			});
 
 		d3.selectAll(".links>line").filter(function(link_d) {
-            return link_d.source.id == d.id || link_d.target.id == d.id;
-          }).style("visibility", "hidden");
+            return (link_d.source.id == d.id && !hidden.includes(link_d.target.id)) || (link_d.target.id == d.id && !hidden.includes(link_d.source.id));
+          }).style("visibility", function(d_sel) {
+          		return ch ? 'visible' : 'hidden';
+          });
 		
-
-		console.log(d);
-		*/
-		/*
-		d3.selectAll("g").style("visibility", function(d_sel) {
-			if(d_sel) {
-				if(d_sel.genres.includes(d)) return 'hidden';
-			}
-		});
-		d3.selectAll(".links>line").style("visibility", function(d_sel) {
-			if(d_sel) {
-				if(d_sel.source.genres.includes(d) || d_sel.target.genres.includes(d)) return 'hidden';
-			}
-		}); 
-	});*/
-
-
-}
-
-
-/*
-	var labels = d3.select("#statlist").selectAll("input")
-		.data(window.genres)
-		.enter().append("label")
-		.attr("class", "col-sm-4 col-md-4 col-lg-4")
-		.text(function(d) { return d; });
-
-	var	boxes = labels.append("input")
-		.attr("type", "checkbox")
-		.attr("checked", "true")
-		.attr("id", function(d) { return d; });
-
-	boxes.on("click", function(d) {
-		console.log(d);
-		d3.selectAll("g").style("visibility", function(d_sel) {
-			if(d_sel) {
-				if(d_sel.genres.includes(d)) return 'hidden';
-			}
-		});
-		d3.selectAll(".links>line").style("visibility", function(d_sel) {
-			if(d_sel) {
-				if(d_sel.source.genres.includes(d) || d_sel.target.genres.includes(d)) return 'hidden';
-			}
-		});
 	});
-	*/
+	/*
+	svg.on("click", function() {
+		d3.selectAll("circle").classed('selected', false);
+	});*/
+}
